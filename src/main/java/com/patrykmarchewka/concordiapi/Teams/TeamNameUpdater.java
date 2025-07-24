c9dd2b5147fd4a77e72173391cc3c9cb0453f9cb
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-07-24 21:07:24 +0200

    Add Javadoc to TeamNameUpdater for improved documentation
    TeamNameUpdater.java
    - Added class and method-level Javadoc

--- src/main/java/com/patrykmarchewka/concordiapi/Teams/TeamNameUpdater.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Teams/TeamNameUpdater.java
@@ -3,12 +3,28 @@ package com.patrykmarchewka.concordiapi.Teams;
 import com.patrykmarchewka.concordiapi.DTO.TeamDTO.TeamRequestBody;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Team;
 
+
+
+/**
+ * Handles updating the {@code name} field of {@link Team} entities
+ */
 public class TeamNameUpdater implements TeamCREATEUpdater,TeamPUTUpdater,TeamPATCHUpdater{
+
+    /**
+     * Sets the team name during team creation
+     * @param team Team being created
+     * @param body TeamRequestBody with data containing team name
+     */
     @Override
     public void CREATEUpdate(Team team, TeamRequestBody body) {
         sharedUpdate(team,body);
     }
 
+    /**
+     * Sets the team name if it's new value is provided in body
+     * @param team Team to edit
+     * @param body TeamRequestBody with data
+     */
     @Override
     public void PATCHUpdate(Team team, TeamRequestBody body) {
         if (body.getName() != null){
@@ -16,11 +32,21 @@ public class TeamNameUpdater implements TeamCREATEUpdater,TeamPUTUpdater,TeamPAT
         }
     }
 
+    /**
+     * Sets the team name to new value
+     * @param team Team to edit
+     * @param body TeamRequestBody with data containing team name
+     */
     @Override
     public void PUTUpdate(Team team, TeamRequestBody body) {
         sharedUpdate(team, body);
     }
 
+    /**
+     * Shared logic for updating team's name
+     * @param team Team to edit
+     * @param body TeamRequestBody with data containing team name
+     */
     void sharedUpdate(Team team, TeamRequestBody body){
         team.setName(body.getName());
     }
