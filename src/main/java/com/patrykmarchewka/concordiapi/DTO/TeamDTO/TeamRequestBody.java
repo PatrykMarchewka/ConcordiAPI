160d585f0744871842a94ffcbe7734a46345d7d8
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-08-17 17:16:54 +0200

    Add validation to TeamRequestBody name field
    TeamRequestBody.java
    - Removed unused import com.fasterxml.jackson.annotation.JsonIgnoreProperties
    - Added imports for .DTO.OnCreate, .DTO.OnPut, jakarta.validation.constraints.NotNull, and jakarta.validation.constraints.Size
    - Annotated name field with @NotNull and @Size
    - Updated @NotBlank annotation on name to include OnCreate and OnPut groups with custom message
    - Removed redundant @JsonIgnoreProperties annotation

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/TeamDTO/TeamRequestBody.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/TeamDTO/TeamRequestBody.java
@@ -1,11 +1,15 @@
 package com.patrykmarchewka.concordiapi.DTO.TeamDTO;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.patrykmarchewka.concordiapi.DTO.OnCreate;
+import com.patrykmarchewka.concordiapi.DTO.OnPut;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-@JsonIgnoreProperties()
 public class TeamRequestBody {
-    @NotBlank
+    @NotNull(groups = {OnCreate.class, OnPut.class},message = "{notnull.name.generic}")
+    @NotBlank(groups = {OnCreate.class, OnPut.class},message = "{notblank.name.generic}")
+    @Size(min = 1, max = 255, message = "{size.generic}")
     private String name;
 
     public TeamRequestBody(String name){
