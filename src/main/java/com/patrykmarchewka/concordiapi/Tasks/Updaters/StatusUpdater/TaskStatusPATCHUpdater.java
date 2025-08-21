91c945f59561c7474c44bdc6ae38bca9f745dfb1
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-08-21 17:20:32 +0200

    Introduce TaskStatusUpdaterHelper for shared logic
    TaskStatusUpdaterHelper.java
    - Created helper class containing shared logic used by TaskStatus updaters
    
    TaskStatusPATCHUpdater.java
    - Added import: org.springframework.beans.factory.annotation.Autowired
    - Refactored to use TaskStatusUpdaterHelper for shared logic
    
    TaskStatusPUTUpdater.java
    - Added import: org.springframework.beans.factory.annotation.Autowired
    - Refactored to use TaskStatusUpdaterHelper for shared logic

--- src/main/java/com/patrykmarchewka/concordiapi/Tasks/Updaters/StatusUpdater/TaskStatusPATCHUpdater.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Tasks/Updaters/StatusUpdater/TaskStatusPATCHUpdater.java
@@ -3,14 +3,23 @@ package com.patrykmarchewka.concordiapi.Tasks.Updaters.StatusUpdater;
 import com.patrykmarchewka.concordiapi.DTO.TaskDTO.TaskRequestBody;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
 import com.patrykmarchewka.concordiapi.Tasks.Updaters.TaskPATCHUpdater;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskStatusPATCHUpdater implements TaskPATCHUpdater {
+
+    private final TaskStatusUpdaterHelper taskStatusUpdaterHelper;
+
+    @Autowired
+    public TaskStatusPATCHUpdater(TaskStatusUpdaterHelper taskStatusUpdaterHelper) {
+        this.taskStatusUpdaterHelper = taskStatusUpdaterHelper;
+    }
+
     @Override
     public void PATCHUpdate(Task task, TaskRequestBody body) {
         if (body.getTaskStatus() != null){
-            task.setTaskStatus(body.getTaskStatus());
+            taskStatusUpdaterHelper.sharedUpdate(task, body);
         }
     }
 }
--- src/main/java/com/patrykmarchewka/concordiapi/Tasks/Updaters/StatusUpdater/TaskStatusPUTUpdater.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Tasks/Updaters/StatusUpdater/TaskStatusPUTUpdater.java
@@ -3,12 +3,21 @@ package com.patrykmarchewka.concordiapi.Tasks.Updaters.StatusUpdater;
 import com.patrykmarchewka.concordiapi.DTO.TaskDTO.TaskRequestBody;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
 import com.patrykmarchewka.concordiapi.Tasks.Updaters.TaskPUTUpdater;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskStatusPUTUpdater implements TaskPUTUpdater {
+
+    private final TaskStatusUpdaterHelper taskStatusUpdaterHelper;
+
+    @Autowired
+    public TaskStatusPUTUpdater(TaskStatusUpdaterHelper taskStatusUpdaterHelper) {
+        this.taskStatusUpdaterHelper = taskStatusUpdaterHelper;
+    }
+
     @Override
     public void PUTUpdate(Task task, TaskRequestBody body) {
-        task.setTaskStatus(body.getTaskStatus());
+        taskStatusUpdaterHelper.sharedUpdate(task, body);
     }
 }
