a4a358483cb5b18d5ef95ec3f841f850e13b21c7
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-08-21 18:41:51 +0200

    Add default values for role and uses in invitation creation
    - Defaulted Role to MEMBER when not provided in POST InvitationRequestBody
    - Defaulted Uses to 1 when not provided in POST InvitationRequestBody
    - Reduced coupling by centralizing defaults within the invitation logic
    
    Why:
    This makes creating invitations easier by not forcing the client to
    always specify role and uses. If they don’t provide values, the system
    falls back to sensible defaults instead of rejecting the request.

--- src/main/java/com/patrykmarchewka/concordiapi/Invitations/Updaters/RoleUpdater/InvitationRoleCREATEUpdater.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Invitations/Updaters/RoleUpdater/InvitationRoleCREATEUpdater.java
@@ -3,21 +3,13 @@ package com.patrykmarchewka.concordiapi.Invitations.Updaters.RoleUpdater;
 import com.patrykmarchewka.concordiapi.DTO.InvitationDTO.InvitationRequestBody;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Invitation;
 import com.patrykmarchewka.concordiapi.Invitations.Updaters.InvitationCREATEUpdater;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.patrykmarchewka.concordiapi.UserRole;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InvitationRoleCREATEUpdater implements InvitationCREATEUpdater {
-
-    private final InvitationRoleUpdaterHelper invitationRoleUpdaterHelper;
-
-    @Autowired
-    public InvitationRoleCREATEUpdater(InvitationRoleUpdaterHelper invitationRoleUpdaterHelper) {
-        this.invitationRoleUpdaterHelper = invitationRoleUpdaterHelper;
-    }
-
     @Override
     public void CREATEUpdate(Invitation invitation, InvitationRequestBody body) {
-        invitationRoleUpdaterHelper.sharedUpdate(invitation, body);
+        invitation.setRole((body.getRole() != null) ? body.getRole() : UserRole.MEMBER);
     }
 }
--- src/main/java/com/patrykmarchewka/concordiapi/Invitations/Updaters/UsesUpdater/InvitationUsesCREATEUpdater.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Invitations/Updaters/UsesUpdater/InvitationUsesCREATEUpdater.java
@@ -3,21 +3,12 @@ package com.patrykmarchewka.concordiapi.Invitations.Updaters.UsesUpdater;
 import com.patrykmarchewka.concordiapi.DTO.InvitationDTO.InvitationRequestBody;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Invitation;
 import com.patrykmarchewka.concordiapi.Invitations.Updaters.InvitationCREATEUpdater;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InvitationUsesCREATEUpdater implements InvitationCREATEUpdater {
-
-    private final InvitationUsesUpdaterHelper invitationUsesUpdaterHelper;
-
-    @Autowired
-    public InvitationUsesCREATEUpdater(InvitationUsesUpdaterHelper invitationUsesUpdaterHelper) {
-        this.invitationUsesUpdaterHelper = invitationUsesUpdaterHelper;
-    }
-
     @Override
     public void CREATEUpdate(Invitation invitation, InvitationRequestBody body) {
-        invitationUsesUpdaterHelper.sharedUpdate(invitation, body);
+        invitation.setUses((body.getUses()) != null ? body.getUses() : 1);
     }
 }
