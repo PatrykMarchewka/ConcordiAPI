4e0b1d2ee944bd89504d2133494dc0aecc1c4df0
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-10-01 17:36:47 +0200

    Refactor TestDataLoader for readability and flexibility
    - Split creation logic into multiple methods
    - Added clearDB method to reset database state
    - Expanded available fields to simplify future test setup
    
    Why:
    To improve test readability, maintainability, and prepare for future testing.

--- src/test/java/com/patrykmarchewka/concordiapi/TestDataLoader.java
+++ src/test/java/com/patrykmarchewka/concordiapi/TestDataLoader.java
@@ -32,15 +32,34 @@ public class TestDataLoader {
 
     public User user1;
     public User user2;
+    public User userAdminA;
+    public User userAdminA2;
+    public User userManagerA;
+    public User userMemberA;
+    public User userAdminB;
+    public User userAdminB2;
+    public User userManagerB;
+    public User userMemberB;
     public Team team1;
     public Team team2;
     public Task task1;
+    public Task task1New;
     public Task task2;
     public Subtask subtask1;
     public Subtask subtask2;
     public Invitation invitation;
+    public Invitation invitationNoUses;
+    public Invitation invitationExpired;
     public String jwt1;
     public String jwt2;
+    public String jwtAdminA;
+    public String jwtAdminB;
+    public String jwtMemberA;
+    public String jwtMemberB;
+
+    public User refreshedUser;
+    public Team refreshedTeam;
+    public Task refreshedTask;
 
     public TestDataLoader(UserService userService, TeamService teamService, TaskService taskService, SubtaskService subtaskService, InvitationService invitationService) {
         this.userService = userService;
@@ -51,26 +70,103 @@ public class TestDataLoader {
     }
 
     public void loadDataForTests(){
+        createUsers();
+        createTeams();
+        addUsersToTeams();
+        createTasks();
+        createSubtasks();
+        createInvitations();
+        setJWTs();
+        getFullEntities();
+    }
+
+    public void clearDB(){
+        invitationService.deleteAll();
+        subtaskService.deleteAll();
+        taskService.deleteAll();
+        teamService.deleteAll();
+        userService.deleteAll();
+    }
+
+    private void createUsers(){
         this.user1 = userService.createUser(new UserRequestBody("string", "string", "string", "string"));
         this.user2 = userService.createUser(new UserRequestBody("login", "password", "name", "lastName"));
+        this.userAdminA = userService.createUser(new UserRequestBody("adminA", "adminA", "adminA", "adminA"));
+        this.userAdminA2 = userService.createUser(new UserRequestBody("adminA2", "adminA2", "adminA2", "adminA2"));
+        this.userManagerA = userService.createUser(new UserRequestBody("managerA", "managerA", "managerA", "managerA"));
+        this.userMemberA = userService.createUser(new UserRequestBody("memberA", "memberA", "memberA", "memberA"));
+        this.userAdminB = userService.createUser(new UserRequestBody("adminB", "adminB", "adminB", "adminB"));
+        this.userAdminB2 = userService.createUser(new UserRequestBody("adminB2", "adminB2", "adminB2", "adminB2"));
+        this.userManagerB = userService.createUser(new UserRequestBody("managerB", "managerB", "managerB", "managerB"));
+        this.userMemberB = userService.createUser(new UserRequestBody("memberB", "memberB", "memberB", "memberB"));
+    }
+
+    private void createTeams(){
         this.team1 = teamService.createTeam(new TeamRequestBody("team"), user1);
         this.team2 = teamService.createTeam(new TeamRequestBody("team2"), user1);
-        this.task1 = taskService.createTask(new TaskRequestBody("task", "taskdesc", Set.of((int)user1.getID()), TaskStatus.NEW), team1);
+    }
+
+    private void addUsersToTeams(){
+        this.team1 = teamService.addUser(team1, userAdminA, UserRole.ADMIN);
+        this.team1 = teamService.addUser(team1, userAdminA2, UserRole.ADMIN);
+        this.team1 = teamService.addUser(team1, userManagerA, UserRole.MANAGER);
+        this.team1 = teamService.addUser(team1, userMemberA, UserRole.MEMBER);
+        this.team2 = teamService.addUser(team2, userAdminB, UserRole.ADMIN);
+        this.team2 = teamService.addUser(team2, userAdminB2, UserRole.ADMIN);
+        this.team2 = teamService.addUser(team2, userManagerB, UserRole.MANAGER);
+        this.team2 = teamService.addUser(team2, userMemberB, UserRole.MEMBER);
+    }
+
+    private void createTasks(){
+        this.task1 = taskService.createTask(new TaskRequestBody("task", "taskdesc", Set.of((int)user1.getID(), (int)userAdminA.getID(), (int)userAdminA2.getID(), (int)userMemberA.getID()), TaskStatus.NEW), team1);
+        this.task1New = taskService.createTask(new TaskRequestBody("taskNew", "taskdescNew", Set.of(), TaskStatus.NEW), team1);
         this.task2 = taskService.createTask(new TaskRequestBody("task2", "taskdesc2", Set.of(), TaskStatus.INPROGRESS), team1);
+    }
+
+    private void createSubtasks(){
         this.subtask1 = subtaskService.createSubtask(new SubtaskRequestBody("subtask", "subtaskdesc", TaskStatus.HALTED), team1.getID(), task1.getID());
         this.subtask2 = subtaskService.createSubtask(new SubtaskRequestBody("subtask2", "subtaskdesc2", TaskStatus.CANCELLED), team1.getID(), task2.getID());
+    }
+
+    private void createInvitations(){
         this.invitation = invitationService.createInvitation(new InvitationRequestBody((short) 101, UserRole.ADMIN, null), team1.getID());
+        this.invitationNoUses = invitationService.createInvitation(new InvitationRequestBody((short)0, UserRole.MANAGER, null), team1.getID());
+        this.invitationExpired = invitationService.createInvitation(new InvitationRequestBody((short)1, UserRole.MEMBER, OffsetDateTimeConverter.nowConverted().minusDays(1)), team1.getID());
+    }
+
+    private void setJWTs(){
         try {
             this.jwt1 = JSONWebToken.GenerateJWToken(user1.getID());
             this.jwt2 = JSONWebToken.GenerateJWToken(user2.getID());
+            this.jwtAdminA = JSONWebToken.GenerateJWToken(userAdminA.getID());
+            this.jwtAdminB = JSONWebToken.GenerateJWToken(userAdminB.getID());
+            this.jwtMemberA = JSONWebToken.GenerateJWToken(userMemberA.getID());
+            this.jwtMemberB = JSONWebToken.GenerateJWToken(userMemberB.getID());
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void getFullEntities(){
         this.user1 = userService.getUserFull(user1);
+        this.userAdminA = userService.getUserFull(userAdminA);
+        this.userAdminA2 = userService.getUserFull(userAdminA2);
+        this.userMemberA = userService.getUserFull(userMemberA);
         this.team1 = teamService.getTeamFull(team1);
         this.task1 = taskService.getTaskFull(task1);
+        this.task1New = taskService.getTaskFull(task1New);
         this.task2 = taskService.getTaskFull(task2);
+    }
 
+    public void refreshUser(User user){
+        this.refreshedUser = userService.getUserFull(user);
+    }
+
+    public void refreshTeam(Team team){
+        this.refreshedTeam = teamService.getTeamFull(team);
+    }
+
+    public void refreshTask(Task task){
+        this.refreshedTask = taskService.getTaskFull(task);
     }
 }
