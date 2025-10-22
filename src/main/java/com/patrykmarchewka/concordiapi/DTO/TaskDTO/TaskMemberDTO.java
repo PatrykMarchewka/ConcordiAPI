cd6f3d3bfdbbe7dfff87dd5a04072ac4cc95b4f3
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-10-22 16:16:01 +0200

    Update TaskMemberDTO to support hydrated TaskFull and future unification with TaskManagerDTO
    - Added new constructor in TaskMemberDTO accepting TaskFull for hydrated task representation
    - Marked creationDate, updateDate, and assignedTeam with @JsonIgnore to prevent serialization
    - Added getter/setter methods for new fields
    - Introduced getCreationDateString() and getUpdateDateString() for formatted date output
    - Updated hashCode() to exclude subtasks and users fields for consistency
    
    Why:
    Part of ongoing effort to migrate toward hydration based DTOs and prepare for unification of TaskMemberDTO and TaskManagerDTO.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskMemberDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskMemberDTO.java
@@ -1,12 +1,18 @@
 package com.patrykmarchewka.concordiapi.DTO.TaskDTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.patrykmarchewka.concordiapi.DTO.SubtaskDTO.SubtaskMemberDTO;
 import com.patrykmarchewka.concordiapi.DTO.UserDTO.UserMemberDTO;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Subtask;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
+import com.patrykmarchewka.concordiapi.DatabaseModel.Team;
 import com.patrykmarchewka.concordiapi.DatabaseModel.User;
+import com.patrykmarchewka.concordiapi.HydrationContracts.Task.TaskFull;
+import com.patrykmarchewka.concordiapi.OffsetDateTimeConverter;
 import com.patrykmarchewka.concordiapi.TaskStatus;
 
+import java.time.OffsetDateTime;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -18,6 +24,12 @@ public class TaskMemberDTO implements TaskDTO {
     private TaskStatus taskStatus;
     private Set<SubtaskMemberDTO> subtasks = new HashSet<>();
     private Set<UserMemberDTO> users = new HashSet<>();
+    @JsonIgnore
+    private OffsetDateTime creationDate;
+    @JsonIgnore
+    private OffsetDateTime updateDate;
+    @JsonIgnore
+    private Team assignedTeam;
 
     public TaskMemberDTO(Task task){
         this.id = task.getID();
@@ -28,9 +40,29 @@ public class TaskMemberDTO implements TaskDTO {
             users.add(new UserMemberDTO(user));
         }
         for (Subtask subtask : task.getSubtasks()){
-            subtasks.add(new SubtaskMemberDTO(subtask));
+            subtasks.add(SubtaskMemberDTO.from(subtask));
         }
+        this.creationDate = task.getCreationDate();
+        this.updateDate = task.getUpdateDate();
+        this.assignedTeam = task.getAssignedTeam();
     }
+
+    public TaskMemberDTO(TaskFull task){
+        this.id = task.getID();
+        this.name = task.getName();
+        this.description = task.getDescription();
+        this.taskStatus = task.getTaskStatus();
+        for (User user : task.getUsers()){
+            users.add(new UserMemberDTO(user));
+        }
+        for (Subtask subtask : task.getSubtasks()){
+            subtasks.add(SubtaskMemberDTO.from(subtask));
+        }
+        this.creationDate = task.getCreationDate();
+        this.updateDate = task.getUpdateDate();
+        this.assignedTeam = task.getAssignedTeam();
+    }
+
 
     public TaskMemberDTO(){}
 
@@ -52,6 +84,20 @@ public class TaskMemberDTO implements TaskDTO {
     public Set<UserMemberDTO> getUsers(){return users;}
     public void setUsers(Set<UserMemberDTO> users){this.users = users;}
 
+    public OffsetDateTime getCreationDate(){return creationDate;}
+    public void setCreationDate(OffsetDateTime creationDate){this.creationDate = creationDate;}
+
+    public OffsetDateTime getUpdateDate(){return updateDate;}
+    public void setUpdateDate(OffsetDateTime updateDate){this.updateDate = updateDate;}
+
+    public Team getAssignedTeam(){return assignedTeam;}
+    public void setAssignedTeam(Team assignedTeam){this.assignedTeam = assignedTeam;}
+
+    @JsonProperty("creationDate")
+    public String getCreationDateString(){return OffsetDateTimeConverter.formatDate(this.creationDate);}
+    @JsonProperty("updateDate")
+    public String getUpdateDateString(){return OffsetDateTimeConverter.formatDate(this.updateDate);}
+
     @Override
     public boolean equals(Object o){
         if (this == o) return true;
@@ -66,5 +112,5 @@ public class TaskMemberDTO implements TaskDTO {
     }
 
     @Override
-    public int hashCode(){ return Objects.hash(id,name,description,taskStatus); }
+    public int hashCode(){ return Objects.hash(id,name,description,taskStatus, subtasks, users); }
 }
