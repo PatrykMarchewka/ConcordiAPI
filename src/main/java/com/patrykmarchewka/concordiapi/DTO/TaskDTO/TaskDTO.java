756ea0791d34dd311536bb25cd0032917f148522
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-11-05 17:17:56 +0100

    Refactor(Task): Remove deprecated equalsTask method from TaskDTO
    - Deleted deprecated equalsTask method from TaskDTO interface as it was no longer used.
    
    Why:
    To clean up unused and unnecessary deprecated code.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskDTO.java
@@ -1,23 +1,10 @@
 package com.patrykmarchewka.concordiapi.DTO.TaskDTO;
 
-import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Task.TaskIdentity;
 
 import java.util.Objects;
 
 public interface TaskDTO extends TaskIdentity {
-    /**
-     * @deprecated Will be replaced by {@link #equalsTask(TaskIdentity)}
-     * @param task
-     * @return
-     */
-    @Deprecated
-    default boolean equalsTask(Task task){
-        return Objects.equals(getID(), task.getID()) &&
-                Objects.equals(getName(), task.getName()) &&
-                Objects.equals(getDescription(), task.getDescription()) &&
-                Objects.equals(getTaskStatus(), task.getTaskStatus());
-    }
 
     default boolean equalsTask(TaskIdentity task){
         return Objects.equals(getID(), task.getID()) &&
