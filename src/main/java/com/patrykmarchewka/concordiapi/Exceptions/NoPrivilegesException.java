9fc65adad2a4c00767a5e3c50f2d7ef5e05cf474
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-11-11 18:22:28 +0100

    Feature(Exception): Add customizable message constructor to NoPrivilegesException
    - Added new constructor to NoPrivilegesException allowing a custom error message.
    
    Why:
    To expand functionality and enable more descriptive error responses when needed.

--- src/main/java/com/patrykmarchewka/concordiapi/Exceptions/NoPrivilegesException.java
+++ src/main/java/com/patrykmarchewka/concordiapi/Exceptions/NoPrivilegesException.java
@@ -2,4 +2,8 @@ package com.patrykmarchewka.concordiapi.Exceptions;
 
 public class NoPrivilegesException extends RuntimeException{
     public NoPrivilegesException(){ super("You are not authorized to do that action"); }
+
+    public NoPrivilegesException(String message){
+        super(message);
+    }
 }
