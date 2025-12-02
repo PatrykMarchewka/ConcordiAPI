daa572845d99abac7ed48ac91edf2fe651801aa8
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-02 23:57:11 +0100

    Refactor(DTO): Apply value based approach to SubtaskMemberDTO
    - Added JSON annotations to SubtaskMemberDTO
    - Updated equals override to also compare task field
    - Updated equals to use == for primitive short and enum instead of Objects.equals
    - Updated hashCode override to include all fields
    
    Why:
    To switch to a value based approach and improve equals and hashCode in DTOs for consistency and clarity.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/SubtaskDTO/SubtaskMemberDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/SubtaskDTO/SubtaskMemberDTO.java
@@ -1,12 +1,15 @@
 package com.patrykmarchewka.concordiapi.DTO.SubtaskDTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Subtask.SubtaskIdentity;
 import com.patrykmarchewka.concordiapi.TaskStatus;
 
 import java.util.Objects;
 
+@JsonPropertyOrder({"ID", "Name", "Description", "Subtask status"})
 public class SubtaskMemberDTO implements SubtaskDTO{
     private long id;
     private String name;
@@ -26,37 +29,47 @@ public class SubtaskMemberDTO implements SubtaskDTO{
     public SubtaskMemberDTO(){}
 
     @Override
+    @JsonProperty("ID")
     public long getID() {return id;}
+    @Override
     public void setID(long id) {this.id = id;}
 
     @Override
+    @JsonProperty("Name")
     public String getName(){return name;}
+    @Override
     public void setName(String name){this.name = name;}
 
     @Override
+    @JsonProperty("Description")
     public String getDescription(){return description;}
+    @Override
     public void setDescription(String description){this.description = description;}
 
     @Override
     public Task getTask(){return task;}
+    @Override
     public void setTask(Task task){this.task = task;}
 
     @Override
+    @JsonProperty("Subtask status")
     public TaskStatus getTaskStatus(){return taskStatus;}
+    @Override
     public void setTaskStatus(TaskStatus taskStatus) {this.taskStatus = taskStatus;}
 
     @Override
     public boolean equals(Object o){
         if (this == o) return true;
         if (!(o instanceof SubtaskMemberDTO subtaskMemberDTO)) return false;
-        return Objects.equals(id, subtaskMemberDTO.getID()) &&
-                Objects.equals(name, subtaskMemberDTO.getName()) &&
-                Objects.equals(description, subtaskMemberDTO.getDescription()) &&
-                Objects.equals(taskStatus, subtaskMemberDTO.getTaskStatus());
+        return id == subtaskMemberDTO.id &&
+                Objects.equals(name, subtaskMemberDTO.name) &&
+                Objects.equals(description, subtaskMemberDTO.description) &&
+                Objects.equals(task, subtaskMemberDTO.task) &&
+                taskStatus == subtaskMemberDTO.taskStatus;
     }
 
     @Override
     public int hashCode(){
-        return Objects.hash(id,name,description,taskStatus);
+        return Objects.hash(id,name,description,task,taskStatus);
     }
 }
