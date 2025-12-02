f42dd8a4016bed43644bbe359a140092b0e44829
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-02 23:55:36 +0100

    Refactor(DTO): Enhance SubtaskDTO interface with setters
    - Added setter methods to SubtaskDTO interface
    - Improved consistency with other DTO interfaces
    
    Why:
    To enhance the interface and align SubtaskDTO with the design of other interfaces.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/SubtaskDTO/SubtaskDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/SubtaskDTO/SubtaskDTO.java
@@ -1,11 +1,20 @@
 package com.patrykmarchewka.concordiapi.DTO.SubtaskDTO;
 
 import com.patrykmarchewka.concordiapi.DatabaseModel.Subtask;
+import com.patrykmarchewka.concordiapi.DatabaseModel.Task;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Subtask.SubtaskIdentity;
+import com.patrykmarchewka.concordiapi.TaskStatus;
 
 import java.util.Objects;
 
 public interface SubtaskDTO extends SubtaskIdentity {
+    void setID(long id);
+    void setName(String name);
+    void setDescription(String description);
+    void setTask(Task task);
+    void setTaskStatus(TaskStatus taskStatus);
+
+
     default boolean equalsSubtask(Subtask subtask){
         return Objects.equals(getID(), subtask.getID()) &&
                 Objects.equals(getName(), subtask.getName()) &&
