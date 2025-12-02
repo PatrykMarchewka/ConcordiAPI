a3aaa7428d56a79282c1bf4f7521dd6f08d7d499
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-02 23:33:27 +0100

    Refactor(DTO): Enhance InvitationDTO interface with setters
    - Added setter methods to InvitationDTO interface
    - Improved consistency with other DTO interfaces
    
    Why:
    To enhance the interface and align InvitationDTO with the design of other interfaces.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/InvitationDTO/InvitationDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/InvitationDTO/InvitationDTO.java
@@ -2,10 +2,16 @@ package com.patrykmarchewka.concordiapi.DTO.InvitationDTO;
 
 import com.patrykmarchewka.concordiapi.DatabaseModel.Invitation;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Invitation.InvitationIdentity;
+import com.patrykmarchewka.concordiapi.UserRole;
 
+import java.time.OffsetDateTime;
 import java.util.Objects;
 
 public interface InvitationDTO extends InvitationIdentity {
+    void setUUID(String UUID);
+    void setUses(short uses);
+    void setRole(UserRole role);
+    void setDueTime(OffsetDateTime dueTime);
 
     default boolean equalsInvitation(Invitation invitation){
         return Objects.equals(getUUID(), invitation.getUUID());
