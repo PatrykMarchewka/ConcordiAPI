7e12b1bc55476e3fe8d3a6998cd9c9dc75180868
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-02 23:50:54 +0100

    Refactor(DTO): Apply value based approach to InvitationManagerDTO
    - Added JSON annotations to InvitationManagerDTO
    - Updated equals override to also compare team field
    - Updated equals override to use == for primitive short instead of Objects.equals
    - Updated equals to use raw fields instead of getter methods
    - Updated hashCode override to include all fields rather than just UUID
    
    Why:
    To switch to a value based approach on equals and hashCode in DTOs for consistency and clarity.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/InvitationDTO/InvitationManagerDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/InvitationDTO/InvitationManagerDTO.java
@@ -2,6 +2,7 @@ package com.patrykmarchewka.concordiapi.DTO.InvitationDTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.patrykmarchewka.concordiapi.DTO.TeamDTO.TeamMemberDTO;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Invitation.InvitationWithTeam;
 import com.patrykmarchewka.concordiapi.OffsetDateTimeConverter;
@@ -10,6 +11,7 @@ import com.patrykmarchewka.concordiapi.UserRole;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
+@JsonPropertyOrder({"UUID", "Team", "Role", "Uses", "Due time"})
 public class InvitationManagerDTO implements InvitationDTO{
 
     private String UUID;
@@ -29,37 +31,51 @@ public class InvitationManagerDTO implements InvitationDTO{
 
     public InvitationManagerDTO(){}
 
+    @Override
+    @JsonProperty("UUID")
     public String getUUID(){return this.UUID;}
+    @Override
     public void setUUID(String UUID){this.UUID = UUID;}
 
+    @JsonProperty("Team")
     public TeamMemberDTO getTeam(){return this.team;}
     public void setTeam(TeamMemberDTO team){this.team = team;}
 
+    @Override
+    @JsonProperty("Role")
     public UserRole getRole(){return this.role;}
+    @Override
     public void setRole(UserRole role){this.role = role;}
 
+    @Override
+    @JsonProperty("Uses")
     public short getUses(){return this.uses;}
+    @Override
     public void setUses(short uses){this.uses = uses;}
 
+    @Override
     public OffsetDateTime getDueTime(){return this.dueTime;}
+    @Override
     public void setDueTime(OffsetDateTime dueTime){this.dueTime = dueTime;}
 
-    @JsonProperty("dueTime")
+    @JsonProperty("Due time")
     public String getDueTimeString(){return OffsetDateTimeConverter.formatDate(this.dueTime);}
+    public void setDueTimeString(String dueTimeString){ this.dueTime = OffsetDateTimeConverter.parseDate(dueTimeString);}
 
 
     @Override
     public boolean equals(Object o){
         if (this == o) return true;
         if (!(o instanceof InvitationManagerDTO invitationManagerDTO)) return false;
-        return Objects.equals(UUID, invitationManagerDTO.getUUID()) &&
-                Objects.equals(role, invitationManagerDTO.getRole()) &&
-                Objects.equals(uses, invitationManagerDTO.getUses()) &&
-                Objects.equals(dueTime, invitationManagerDTO.getDueTime());
+        return Objects.equals(UUID, invitationManagerDTO.UUID) &&
+                Objects.equals(team, invitationManagerDTO.team) &&
+                Objects.equals(role, invitationManagerDTO.role) &&
+                uses == invitationManagerDTO.uses &&
+                Objects.equals(dueTime, invitationManagerDTO.dueTime);
     }
 
     @Override
     public int hashCode(){
-        return Objects.hash(UUID);
+        return Objects.hash(UUID, team, role, uses, dueTime);
     }
 }
