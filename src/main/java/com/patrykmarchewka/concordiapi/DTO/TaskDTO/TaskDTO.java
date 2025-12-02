0508e5d146c599cd8bbb9d031c820141199fa872
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-03 00:08:32 +0100

    Refactor(DTO): Enhance TaskDTO interface with setters
    - Added setter methods to TaskDTO interface
    - Improved consistency with other DTO interfaces
    
    Why:
    To enhance the interface and align TaskDTO with the design of other interfaces.

--- src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskDTO.java
+++ src/main/java/com/patrykmarchewka/concordiapi/DTO/TaskDTO/TaskDTO.java
@@ -1,10 +1,20 @@
 package com.patrykmarchewka.concordiapi.DTO.TaskDTO;
 
+import com.patrykmarchewka.concordiapi.DatabaseModel.Team;
 import com.patrykmarchewka.concordiapi.HydrationContracts.Task.TaskIdentity;
+import com.patrykmarchewka.concordiapi.TaskStatus;
 
+import java.time.OffsetDateTime;
 import java.util.Objects;
 
 public interface TaskDTO extends TaskIdentity {
+    void setID(long ID);
+    void setName(String name);
+    void setDescription(String description);
+    void setTaskStatus(TaskStatus taskStatus);
+    void setCreationDate(OffsetDateTime creationDate);
+    void setUpdateDate(OffsetDateTime updateDate);
+    void setAssignedTeam(Team assignedTeam);
 
     default boolean equalsTask(TaskIdentity task){
         return Objects.equals(getID(), task.getID()) &&
