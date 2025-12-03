83890677e6390bc42a7d47968dcac2498030a3d1
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2025-12-03 19:54:46 +0100

    refactor(interface): remove unused hasUser method from TaskWithUserTasks
    - Removed default boolean hasUser(User user) from TaskWithUserTasks interface as it is no longer used
    
    Why:
    To clean up old unused code.

--- src/main/java/com/patrykmarchewka/concordiapi/HydrationContracts/Task/TaskWithUserTasks.java
+++ src/main/java/com/patrykmarchewka/concordiapi/HydrationContracts/Task/TaskWithUserTasks.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 public interface TaskWithUserTasks extends TaskIdentity{
     Set<UserTask> getUserTasks();
     default Set<User> getUsers() {return getUserTasks().stream().map(UserTask::getAssignedUser).collect(Collectors.toUnmodifiableSet());}
-    default boolean hasUser(User user){return getUserTasks().stream().map(UserTask::getAssignedUser).anyMatch(u -> u.equals(user));}
     default boolean hasUser(long userID){return getUserTasks().stream().anyMatch(userTask -> userTask.getAssignedUser().getID() == userID);}
 
 }
